79a57de6707ca3c3458a4d93c008fb4237abd0fe
Ystalard <58308727+Ystalard@users.noreply.github.com>
2022-09-14 17:14:52 +0200

    animation cercle
    animation cercle

--- js/navButtonConnector.js
+++ js/navButtonConnector.js
@@ -1,25 +1,44 @@
 window.addEventListener('load', (event) => {
     var nav = document.querySelector("nav");
     if(nav != null){
-      var rightElements = nav.querySelectorAll("div.rightConnector");
-      rightElements.forEach(function(rightConnector){
+      var elements = nav.querySelectorAll("div.connector");
+      elements.forEach(function(connector){
           var a = window.getComputedStyle(nav).getPropertyValue('--offsetCenter');
           var b = window.getComputedStyle(nav).getPropertyValue('--radius');
-          var angle = window.getComputedStyle(nav).getPropertyValue('--angle');
-          var heightConnector = window.getComputedStyle(rightConnector).getPropertyValue('--height');
+          var button = connector.nextElementSibling
+          var angle = window.getComputedStyle(button).getPropertyValue('--angle');
+          
           var gamma = 180 - Math.abs(getValue(angle))
           var c = alKashi_c_edge(getValue(a),getValue(b),degrees_to_radians(gamma))
           var beta = alKashi_beta_angle(getValue(a),getValue(b),c)
           var betaDegree = radians_to_degrees(beta)
-          var translation = getValue(heightConnector)/2
-          var transformation = "rotate(" + betaDegree + "deg) translateY(-"+ translation + getUnit(heightConnector)+") translateX("+ c/2 +"px)"
-          rightConnector.style.width = "48px";
-          rightConnector.style.transform = transformation
+
+          var heightConnector = window.getComputedStyle(connector).getPropertyValue('--height');
           
-          // rightConnector.clientWidth = alKashi(a.substring(1,a.length -2),b.substring(1,b.length -2),angle.substring(1,angle.length -3));
+          if (angleOutSide_0_PI(getValue(angle))){
+            betaDegree = -betaDegree
+          }
+
+          var translation = getValue(heightConnector)/2
+          var menuWidth = window.getComputedStyle(nav).getPropertyValue('--menuWidth');
+          var buttonWidth = window.getComputedStyle(button).getPropertyValue('--buttonWidth');
+          var finalTranslation = getValue(menuWidth)/2 + (c-getValue(menuWidth)/2 - getValue(buttonWidth)/2)/2
+          var transformation = "rotate(" + betaDegree + "deg) translateY(-"+ translation + getUnit(heightConnector)+") translateX("+ finalTranslation + getUnit(menuWidth) +")"
+          
+          connector.style.transform = transformation
+          
+          var borderSize = window.getComputedStyle(connector).getPropertyValue('--borderSize');
+          connector.style.setProperty("--width",c - getValue(menuWidth)/2 -getValue(buttonWidth)/2 + 2*getValue(borderSize) + getUnit(menuWidth));
         });
     }
   });
+
+function angleOutSide_0_PI(angle){
+  var radian = degrees_to_radians(angle)
+  var normalizedRadian = radian % (2*Math.PI)
+  if (0 <= normalizedRadian && normalizedRadian <= Math.PI ) return false
+  return true
+}
 
 function alKashi_c_edge(a,b,gamma){
     return Math.sqrt(a*a + b*b - 2*a*b*Math.cos(gamma));
@@ -41,7 +60,7 @@ function radians_to_degrees(radians){
 }
 
 function getValue(parameter){
-  return parameter.match(/(\d+)([a-zA-Z]+)/)[1]
+  return parameter.match(/(-?\d+)([a-zA-Z]+)/)[1]
 }
 
 function getUnit(parameter){
